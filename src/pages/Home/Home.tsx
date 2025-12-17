8f8c2af173be1cc2757274141b633fb18718524b
JesseChum <jessechum@icloud.com>
2025-12-16 22:23:53 -0800

    Added in tailwind css and added experimental css for display

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Auburn Airsoft Community Field</h1>
-      <p>Welcome to AACF</p>
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+        <div className="text-center">
+      <h1 className="text-4xl font-bold mb-2">Auburn Airsoft Community Field</h1>
+      <p className="text-gray-300">Welcome to AACF</p>
+      </div>
     </div>
   )
 }
