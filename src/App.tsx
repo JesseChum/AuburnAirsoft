036380aa16fd7994864f88710c824c1d54da0282
JesseChum <jessechum@icloud.com>
2025-12-20 19:15:04 -0800

    Created the Events page itself and adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from "./components/Footer/Footer"
 
+
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Rules from './pages/Rules/Rules'
 import Waiver from './pages/Waiver/Waiver'
+import Events from "./pages/Events/Events"
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="/waiver" element={<Waiver />} />
+        <Route path="/events" element={<Events /> } />
       </Routes>
       <Footer/>
     </>
