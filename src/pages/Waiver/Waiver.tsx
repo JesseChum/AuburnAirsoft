c9ce4d32111f588ec8c54dba05d3ae04546a8bab
JesseChum <jessechum@icloud.com>
2025-12-23 16:34:12 -0800

    imported image and airsoftwaiver pdf. created the main page for the waiver and working on the waiverviewer.

--- src/pages/Waiver/Waiver.tsx
+++ src/pages/Waiver/Waiver.tsx
@@ -1,8 +1,11 @@
+import WaiverViewer from "../../components/Waiver/WaiverViewer"
+
 export default function Waiver() {
     return (
          <main className="min-h-screen bg-zinc-900 text-white px-6 py-12">
             <div className="max-w-4xl mx-auto space-y-10">
-                <h1>WAIVER COMING SOON</h1>
+                <h1 className="text-3xl font-bold">Player Waiver</h1>
+                <WaiverViewer />
             </div>
          </main>
     )
