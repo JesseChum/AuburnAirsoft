6d5f4b1df1b5d8dab4cab8927057c5addd018e85
JesseChum <jessechum@icloud.com>
2025-12-24 17:51:59 -0800

    Added the rest of the rules. Need to add a fps/joule chart and read over everything again

--- src/pages/Rules/Rules.tsx
+++ src/pages/Rules/Rules.tsx
@@ -23,11 +23,15 @@ export default function Rules() {
         <RuleSection
           title="General Rules"
           rules={[
-            "All airsoft guns must be concealed until in the staging area.",
-            "Real firearms, knives, tasers, paintball guns, fireworks, metal BB guns, and airguns are prohibited.",
-            "No drugs or alcohol of any kind are permitted.",
+            "All airsoft guns must be concealed in a gun case, bag, or original box until in the staging area.",
+            "Real firearms, knives, tasers, paintball guns, fireworks, metal BB guns, and airguns, and other non-airsoft weapons or devices are not permitted during airsoft game play.",
+            "ALL Drugs and alcohol in all forms are not permitted. You will be asked to leave if suspected of being under the influence of drugs or alcohol.",
             "Players must clean up after themselves. Trash bags and gloves will be provided.",
             "No blind firing. You must visually identify your target and backdrop.",
+            "Field boundaries will be represented with caution tape and red marking tape. Field Organizer will explain the boundaries in person.",
+            "Teams will be separated with arm bands and no arm bands via red marking tape. If you have your own identification bands please bring them.",
+            "If you see or encounter a house stay at least 300+ feet away from the house/property. If it's hard to measure, make sure there's no way your gun can reach.",
+            "Only Red lasers are permitted to be used on the field. Class 2 lasers are only permitted,Class 3R and 3B are permitted only for night games. Class 4 is not permitted"
           ]}
         />
         {/* Hit Rules */}
@@ -40,6 +44,7 @@ export default function Rules() {
             "Friendly fire counts.",
             "Ricochets count.",
             "Gun hits do NOT count.",
+            "Dead Rags are Highly Recommended. Typically in a bright visible color",
           ]}
         />
 
@@ -51,6 +56,50 @@ export default function Rules() {
             "Smoke and grenades are permitted if field conditions allow.",
             "Grenades without BBs are distraction devices.",
             "BB grenades have a 20ft kill radius unless behind hard cover.",
+          ]}
+        />
+
+        {/* Pyrotechnics */}
+        <RuleSection
+          title="Honor and Integrity"
+          rules={[
+            "Cheating will be dealt with in a fair but harsh manner.",
+            "Do not whatsoever have any kind of aggressive physical confrontation with any other person.",
+            "Any kind of threats for this matter are also not allowed and you will be removed from the game.",
+            "Talk to an admin before escalating the situation. Involve an admin before involving yourself. If the conflict does escalate, admins will act as they see fit.",
+          ]}
+        />
+
+        {/* Chrono and Velocity */}
+        <RuleSection
+          title="Chrono and Velocity || Energy Limits || BB type"
+          rules={[
+            "It is highly encouraged to use BIO bbs but not a requirement.",
+            "Plastic BBs ONLY. No metal core plastic BBs.",
+            "No sound amplifiers for neighbors courtesy.",
+            "Full auto is allowed, Guns including Pistols, Shotguns, rifles, machine guns,Unique guns with bursts are acceptable, unless specifically stated in a game mode not to use it.",
+            "RPS limit: 25. No 40mikes or any device that is higher than the limit",
+            "Close range - Pistols, Shotguns || -350 FPS w/2’s or 1.14 Joules",
+            "All Rifles Except for DMRs and Bolt Action Sniper Rifles || - 400 FPS full auto w/.2's or 1.49 Joules maximum",
+            "Or - 450 FPS semi only w/.2’s or 2 Joules maximum",
+            "DMR must be fixed on semi-auto only",
+            "Sniper Rifles or ANY gun over 400 FPS or 1.49 Joules || - 550fps with .2's OR 2.81 Joules",
+            "HIgh Pressure Air/HPA || All HPA guns must have a tournament lock."
+
+            
+          ]}
+        />
+
+        {/* Eye Protection */}
+        <RuleSection
+          title="Eye Protection"
+          rules={[
+            "Eye Protection is NOT to be removed during game play. DO NOT remove your mask or eye  protection for any reason during game play. If there is a malfunction in your eye protection call “blind man” and remove yourself from play, readjust your eye protection, then re-engage.",
+            "Eye protection must have a rubber/foam/soft material seal that closely conforms to your facial features.",
+            "Eye protection should be ANZI Z87.1 compliant or better. This standard is usually posted either on the packaging or on the manufacturer's website.",
+            "Mesh goggles should be made from stamped steel and not deform from repeated close range hits.",
+            "Failure to follow the guidelines on eye protection will mean that you will be asked to leave the game and get the appropriate protection.",
+            "All Operators under 18 must wear one of the following mouth protections: Neoprene Ski Mask, Sports mouth Guard, Lower Stamped Steel, or Paintball/ Airsoft face mask. No Exceptions."
           ]}
         />
           </div>
